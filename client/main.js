93c9ca8c960700851f3e8ce150a1a21b476f3bcd
panbhatt <panbhatt@gmail.com>
2015-12-07 17:57:01 -0500

    Added unfinished table... for editing properties
    Added unfinished table... for editing properties

--- client/main.js
+++ client/main.js
@@ -39,8 +39,10 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
 });
 
 // Make sure the things for the XEditable option. 
-app.run(function(editableOptions) {
-  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+app.run(function(editableOptions, editableThemes) {
+  editableThemes.bs3.inputClass = 'input-sm';
+  editableThemes.bs3.buttonsClass = 'btn-sm';
+  editableOptions.theme = 'bs3';
 });
 
 /**
@@ -152,6 +154,67 @@ app.controller('PageCtrl', function ($scope,ModalService) {
             });
 
     };
+    
+      $scope.users = [
+        {id: 1, name: 'awesome user1', status: 2},
+        {id: 2, name: 'awesome user2', status: undefined},
+        {id: 3, name: 'awesome user3', status: 2}
+      ]; 
+
+      $scope.statuses = [
+        {value: 1, text: 'status1'},
+        {value: 2, text: 'status2'},
+        {value: 3, text: 'status3'},
+        {value: 4, text: 'status4'}
+      ]; 
+
+    $scope.groups = [];
+    
+     $scope.showGroup = function(user) {
+    if(user.group && $scope.groups.length) {
+      var selected = $filter('filter')($scope.groups, {id: user.group});
+      return selected.length ? selected[0].text : 'Not set';
+    } else {
+      return user.groupName || 'Not set';
+    }
+  };
+
+  $scope.showStatus = function(user) {
+    var selected = [];
+    if(user.status) {
+      selected = $filter('filter')($scope.statuses, {value: user.status});
+    }
+    return selected.length ? selected[0].text : 'Not set';
+  };
+
+  $scope.checkName = function(data, id) {
+    if (id === 2 && data !== 'awesome') {
+      return "Username 2 should be `awesome`";
+    }
+  };
+
+  $scope.saveUser = function(data, id) {
+    //$scope.user not updated yet
+    angular.extend(data, {id: id});
+    return $http.post('/saveUser', data);
+  };
+
+  // remove user
+  $scope.removeUser = function(index) {
+    $scope.users.splice(index, 1);
+  };
+    
+    $scope.addUser = function() {
+        $scope.inserted = {
+          id: $scope.users.length+1,
+          name: '',
+          status: 2,
+          group: null 
+        };
+        $scope.users.push($scope.inserted);
+      };
+    
+      
 
 
 });
